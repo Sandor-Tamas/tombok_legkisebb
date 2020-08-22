0bcd236d23a6c1cc3e92d0385ad8a6e5b23d4ae5
Sandor-Tamas <70057013+Sandor-Tamas@users.noreply.github.com>
2020-08-22 21:16:52 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
 let list: number[] = []
 for (let index = 0; index <= 5; index++) {
     list[index] = randint(0, 9)
-}
-for (let index = 0; index <= 5; index++) {
     basic.showNumber(list[index])
 }
-let i = 0
-let min = list[i]
-for (let index = 0; index < 4; index++) {
+let i = 1
+let min = list[0]
+for (let index = 0; index < 5; index++) {
     if (min > list[i]) {
         min = list[i]
     }
